097d9a906e100fd4cd550d9ddae5f4300d1d492a
Gabor Papp <gabor.papp@gmail.com>
2011-07-31 21:57:11 +0200

    added perceptual visualization test

---
+++ facade-control/perceptual.scm
@@ -0,0 +1,40 @@
+(require racket/vector)
+(require fluxus-018/fluxus-sonotopy)
+(require "facade-control.ss")
+
+(clear)
+
+(define host "169.254.73.149")
+(texture-params 0 '(min nearest mag nearest))
+
+(set-camera-transform (mtranslate #(0 0 -37)))
+
+(fc-init host)
+
+(init-sonotopy)
+(grid-size (vector fc-pixels-width fc-pixels-height 0))
+
+; flattens the 2d grid
+(define (vector2d->vector1d v)
+    (apply vector-append (vector->list v)))
+
+(define (mainloop)
+    (let ([pattern (vector2d->vector1d (grid-pattern))])
+        (with-primitive fc-pixels
+            (pdata-index-map!
+                (λ (i c)
+                  (vclamp (vadd #(0.01 0.01 0.01)
+                                (vmul #(1 0 0)
+                                      (expt (vector-ref pattern i) 10.0)))))
+                "c")
+            (pixels-upload)))
+    (fc-update))
+
+(with-primitive fc-pixels
+    (identity)
+    (scale (vector fc-pixels-width (- fc-pixels-height) 1))
+    (hint-cull-ccw)
+    (hint-wire))
+
+(every-frame (mainloop))
+
