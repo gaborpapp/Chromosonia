56edf27a926b101be300e03ac0d3a5843eabc8d3
chromosonia <chromosonia@Hangyas-Mac-mini.local>
2011-09-03 21:50:38 +0200

    added start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# /usr/local/bin/./jackdmp -R -d coreaudio -r 44100 -p 1024 -o 2 -i 2 -d ~:Aggregate:0
+killall jackdmp
+killall fluxus
+jackd -d coreaudio -p 1024 &
+fluxus -fs -x chromosonia.scm &
+sleep 10
+jack_connect system:capture_1 system:playback_1
+jack_connect system:capture_2 system:playback_2
+jack_connect fluxus:in system:capture_1
+
